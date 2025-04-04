d5516ae4cfb0d04e342040b9c35bc60d57272088
rafaela-eduarda <149601165+rafaela-eduarda@users.noreply.github.com>
2025-04-04 14:41:51 -0300

    Add files via upload

---
+++ src/telas/Home/index.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import Topo from './componentes/Topo';
+import Produtores from './componentes/Produtores';
+
+export default function Home(){
+    return <>
+        <Topo />
+        <Produtores />
+        </>
+}
